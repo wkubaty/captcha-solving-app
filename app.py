c942e3e0ff1046663482625315189c4a9f706388
Wojciech Kubaty <wkubaty@gmail.com>
2019-06-22 15:47:45 +0200

    Display captcha and some stats

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
+import random
+
+CAPTCHAS_DIR = os.path.join('static', 'img', 'captchas')
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+@app.route('/index')
+def show_index():
+    number = random.randint(0, 9)
+    filename = os.path.join(CAPTCHAS_DIR, 'about_{}.png'.format(number))
+    labels = ["about", "black", "chance"]
+    values = [0.9, 0.08, 0.02]
+    return render_template("index.html", user_image=filename, labels=labels, values=values)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8080)
---
+++ static/js/chart.js
@@ -0,0 +1,19 @@
+function renderChart(labels, values) {
+    new Chart(document.getElementById("stats-chart"), {
+        type: 'horizontalBar',
+        data: {
+            labels: labels,
+            datasets: [{
+                backgroundColor: ["#FF0000", "#FF8C00", "#FFD700", "#ADFF2F", "#20B2AA"],
+                data: values
+            }
+            ]
+        },
+        options: {
+            legend: {display: false},
+            title: {
+                display: true
+            }
+        }
+    });
+}
