b58910312290edb0c6ea0eefccfed2f5a655a7ec
Wojciech Kubaty <wkubaty@gmail.com>
2019-06-25 03:27:50 +0200

    Replace random with inputs

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import random
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 from models.captcha import Captcha
 from services.generator import Generator
@@ -17,29 +17,30 @@ n_words = 100
 duplicates = 5
 words = load_words(captcha_source)[:n_words]
 query = Query(words, "captcha", "http://127.0.0.1:1234/invocations")
-
+generator = Generator(captchas_dir, words)
+generator.generate_captcha_images(duplicates=duplicates)
 print("Recognizable words: ", words)
 
 
 @app.route('/')
 @app.route('/index')
 def index():
+    word = request.args.get('word', type=str)
     random_word = words[random.randint(0, n_words - 1)]
     random_duplicate = random.randint(0, duplicates - 1)
+    if word:
+        generator.generate_single_captcha_image(word, random_duplicate)
+        random_word = word
+
     filename = os.path.join(captchas_dir, '{}_{}.png'.format(random_word, random_duplicate))
+
     captcha = Captcha(filename)
     data_json = captcha.convert_image_to_json()
     # labels, values = query.query_endpoint("captcha", data_json)[0]
     labels, values = query.query_local(data_json)
 
-    return render_template("index.html", user_image=filename, labels=labels, values=values)
-
-
-def init():
-    generator = Generator(captchas_dir, words)
-    generator.generate_captcha_images(duplicates=duplicates)
+    return render_template("index.html", words=words, captcha_word=random_word, user_image=filename, labels=labels, values=values)
 
 
 if __name__ == '__main__':
-    init()
     app.run(host='0.0.0.0', port=8080)
--- services/generator.py
+++ services/generator.py
@@ -13,3 +13,8 @@ class Generator:
         for word in self.words:
             for i in range(duplicates):
                 image.write(word, os.path.join(self.path, '{}_{}.png'.format(word, i)))
+
+
+    def generate_single_captcha_image(self, word, random_duplicate):
+        image = ImageCaptcha(width=160, height=60, font_sizes=[44])
+        image.write(word, os.path.join(self.path, '{}_{}.png'.format(word, random_duplicate)))
